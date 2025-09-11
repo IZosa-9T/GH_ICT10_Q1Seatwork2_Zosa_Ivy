25e304dba8b2b0bf2a84d6418a1cdf84e6b81669
Ivy <zosa.ia@obmontessori.edu.ph>
2025-09-11 09:38:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ popular_item_price = 59.0 # floating-point
 
 product_flavor = 'Vanilla' # script
 product_names = ['Ice cream', 'Cake', 'Milkshake', 'Yogurt', 'Pudding'] # list
-product_prices = 59.0, 70.0, 87.0, 105.0, 187.0 # tuple
+menu_prices = 59.0, 70.0, 87.0, 105.0, 187.0 # tuple
 common_allergens = 'Milk, Eggs, Nuts' # script
 tax_rate = 0.09 # floating-point
 has_delivery = True
@@ -27,19 +27,19 @@ display(f'Since {year_established}', target='yearestablished')
 display(f'tax rate : {tax_rate} ₱', target='taxrate')
 
 display(f'{product_flavor} {product_names[0]}', target='Ice_cream_product')
-display(f'{product_prices[1]} ₱', target='Ice_cream_price')
+display(f'{menu_prices[1]} ₱', target='Ice_cream_price')
 
 display(f'{product_flavor} {product_names[1]}', target='Cake_product')
-display(f'{product_prices[4]} ₱', target='Cake_price')
+display(f'{menu_prices[4]} ₱', target='Cake_price')
 
 display(f'{product_flavor} {product_names[2]}', target='Milkshake_product')
 display(f'{popular_item_price} ₱', target='Milkshake_price')
 
 display(f'{product_flavor} {product_names[3]}', target='Yogurt_product')
-display(f'{product_prices[2]} ₱', target='Yogurt_price')
+display(f'{menu_prices[2]} ₱', target='Yogurt_price')
 
 display(f'{product_flavor} {product_names[4]}', target='Pudding_product')
-display(f'{product_prices[3]} ₱', target='Pudding_price')
+display(f'{menu_prices[3]} ₱', target='Pudding_price')
 
 if has_delivery:
     display("Delivery available !", target="deliverystatus");
